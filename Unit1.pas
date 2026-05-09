51fb97955a7d92475266ef03a8a914956f833b2c
drbor314 <borrmann-gmbh@t-online.de>
2026-05-09 19:29:54 +0200

    Add files via upload

---
+++ Unit1.pas
@@ -0,0 +1,32 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+Label1.Caption:='Hallo';
+end;
+
+end.
